ccd3cab27e4a3f4300af30477043cb240716fd8e
Daniel Campos <danielcampos14@hotmail.com.br>
2020-11-02 19:02:44 -0300

    Implementações
    lerCache, escreverCache, escreverArqTemp, escreverArqFinal

--- include/Computador/CPU.hpp
+++ include/Computador/CPU.hpp
@@ -1,20 +1,19 @@
 #ifndef CPU_H
 #define CPU_H
-#define ARQFINAL result.txt
-#define ARQTEMP resultTemp.txt
 #include <string>
+#include <fstream> 
 //#include "MemCache.hpp"
 namespace Computador{
 	class CPU{
 	public:
 		void lerCache(unsigned int posicao);
 		void escreverCache(unsigned int posicao, std::string dado);
+		void escreverArqFinal();
 		~CPU();
 		CPU();
 	private:
-		void escreverArqTemp();
-		void escreverArqFinal();
-		//MemCache* memCache;
+		void escreverArqTemp(std::string linha);
+		//Computador::MemCache* memCache;
 		unsigned int leituras;
 		unsigned int escritas;
 		unsigned int misses;
--- src/Computador/CPU.cpp
+++ src/Computador/CPU.cpp
@@ -7,29 +7,82 @@ namespace Computador{
 		this->escritas=0;
 		this->misses=0;
 		this->hits=0;
+		//this->memCache = new Computador::MemCache();
 	}
 
 	CPU::~CPU(){
 		//deleta a memória cache
+		//delete this->memCache;
 	}
 
 	//Operação de leitura da Cache
 	void CPU::lerCache(unsigned int posicao){
-
+		this->leituras++;
+		bool hit=false;
+		//Se deu hit
+		if(this->memCache->buscarMemDados(posicao)){
+			this->hit++;
+			hit = true;
+		}else{
+			this->misses++;
+		}
+		//Escreve no arqTemp
+		//posicao 0 hit ? H : M
+		std::string linha = posicao;
+		linha.append(" 0 ");
+		if(hit){
+			linha.append("H");
+		}else{
+			linha.append("M");
+		}
+		escreverArqTemp(linha);
 	}
 
 	//Operação de escrita na cache
 	void CPU::escreverCache(unsigned int posicao, std::string dado){
-
+		this->escritas++;
+		this->memCache->salvarMemDados(posicao,dado);
+		//Escreve no arqTemp
+		//posicao 1 dado W
+		std::string linha = posicao;
+		linha.append(" 1 ");
+		linha.append(dado);
+		linha.append(" W");
+		escreverArqTemp(linha);
 	}
 
 	//Escreve o arquivo temporário
-	void CPU::escreverArqTemp(){
-
+	void CPU::escreverArqTemp(std::string linha){
+		std::fstream saida;
+		saida.open("../../output/resultTemp.txt", std::ofstream::app);
+		saida<<linha<<std::endl;
+		saida.close();
 	}
 
 	//Escreve o arquivo final de saída
 	void escreverArqFinal(){
+		std::fstream saida, arqTemp;
+		saida.open("../../output/result.txt", std::ofstream::out);
 
+		saida<<"READS: "<<this->leituras<<std::endl;
+		saida<<"WRITES: "<<this->escritas<<std::endl;
+		saida<<"HITS: "<<this->hits<<std::endl;
+		saida<<"MISSES: "<<this->misses<<std::endl;
+
+		float hitRate = this->hits/this->leituras;
+		float missRate = this->misses/this->leituras;
+		saida<<"HIT RATE: "<<hitRate<<std::endl;
+		saida<<"MISS RATE: "<<missRate<<std::endl;
+		saida<<std::endl;
+
+		arqTemp.open("../../output/resultTemp.txt", std::ofstream::in);
+		while(!arqTemp.eof()){
+			std::string linha;
+			arqTemp>>linha;
+			saida<<linha<<std::endl;
+		}
+		
+		arqTemp.close();
+		saida.close();
 	}
 }
